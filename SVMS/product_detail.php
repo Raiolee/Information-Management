3cc7ebc82253b39c5895dec3a414eb6bb3849cc0
Jonr1s <149182212+Jonr1s@users.noreply.github.com>
2023-11-06 21:08:54 +0800

    Update product_detail.php

--- SVMS/product_detail.php
+++ SVMS/product_detail.php
@@ -21,7 +21,7 @@ if (isset($_GET["product_id"]))
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
 
     <!-- Header -->
-  <?php include('header.php'); ?>
+  <?php include('includes/header.php'); ?>
 </head>
 
 <body>
