5032ac5f8a0f960d18b3f5b1942e648e3f3d25af
Jonr1s <149182212+Jonr1s@users.noreply.github.com>
2023-11-06 20:51:28 +0800

    Update homepage.php

--- SVMS/homepage.php
+++ SVMS/homepage.php
@@ -1,3 +1,10 @@
+<?php
+require("config/connection.php");
+
+$selectQuery = "Select * from product;";
+$doSelectquery = mysqli_query($connection, $selectQuery);
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,6 +13,18 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+  
+  <!-- Style for homepage short description -->
+  <style>
+  .custom-line-clamp {
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    -webkit-line-clamp: 3; 
+  }
+    
+</style>
   <!-- Header -->
   <?php include('includes/header.html'); ?>
 </head>
@@ -16,78 +35,24 @@
   <div class="d-flex justify-content-center">
     <div class="container">
       <div class="row gap-3">
+        
+      <?php
+         while ($row = mysqli_fetch_array($doSelectquery))
+         {
+      ?>
         <div class="card my-3" style="width: 18rem;">
-          <img src="https://picsum.photos/200/200" class="card-img-top mt-3 rounded" alt="...">
+          <img src=<?php echo $row ['image_details'] ?> class="card-img-top mt-3 rounded" alt="...">
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-              content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <h5 class="card-title"><?php echo $row ['product_name'] ?></h5>
+            <p class="card-text custom-line-clamp"><?php echo $row ['product_desc'] ?></p>
+            <a href="product_detail.php?product_id=<?php echo $row['product_id']; ?>" class="btn btn-primary">Go somewhere</a>
           </div>
         </div>
-        <div class="card my-3" style="width: 18rem;">
-          <img src="https://picsum.photos/200/200" class="card-img-top mt-3 rounded" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-              content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
-        </div>
-        <div class="card my-3" style="width: 18rem;">
-          <img src="https://picsum.photos/200/200" class="card-img-top mt-3 rounded" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-              content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
-        </div>
-        <div class="card my-3" style="width: 18rem;">
-          <img src="https://picsum.photos/200/200" class="card-img-top mt-3 rounded" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-              content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
-        </div>
-        <div class="card my-3" style="width: 18rem;">
-          <img src="https://picsum.photos/200/200" class="card-img-top mt-3 rounded" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-              content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
-        </div>
-        <div class="card my-3" style="width: 18rem;">
-          <img src="https://picsum.photos/200/200" class="card-img-top mt-3 rounded" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-              content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
-        </div>
-        <div class="card my-3" style="width: 18rem;">
-          <img src="https://picsum.photos/200/200" class="card-img-top mt-3 rounded" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-              content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
-        </div>
-        <div class="card my-3" style="width: 18rem;">
-          <img src="https://picsum.photos/200/200" class="card-img-top mt-3 rounded" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-              content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
-        </div>
+
+      <?php
+         }
+      ?>
+
       </div>
     </div>
   </div>
