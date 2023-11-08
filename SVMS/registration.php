03615c574ab97207ec92812781b66d5724f5ceee
Raiolee <114850923+Raiolee@users.noreply.github.com>
2023-11-08 18:12:13 +0800

    Update registration.php

--- SVMS/registration.php
+++ SVMS/registration.php
@@ -4,44 +4,28 @@ $page_title = "Register";
 require_once 'config/connection.php';
 
 $errors = array();
-$target = "pictures/image";
-$targetFile = $target . basename($_FILES["image"]["name"]);
-$isOk = false;
 
-if (file_exists($targetFile)) {
-  die("File Exists");
-}
-if (move_uploaded_file($_FILES["image"]["temp_name"], $targetFile)) {
-  $isOk = true;
-}
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // Validate and sanitize user inputs...
+  $email = mysqli_real_escape_string($connection, $_POST["email"]);
+  $password = mysqli_real_escape_string($connection, $_POST["password"]);
+  $fname = mysqli_real_escape_string($connection, $_POST["fname"]);
+  $lname = mysqli_real_escape_string($connection, $_POST["lname"]);
+  $mobilenumber = mysqli_real_escape_string($connection, $_POST["mobilenumber"]);
+  $gender = mysqli_real_escape_string($connection, $_POST["gender"]);
+  $bday = $_POST['dob'];
+  $address = mysqli_real_escape_string($connection, $_POST["address"]);
+  $type = mysqli_real_escape_string($connection, $_POST["usertype"]);
+  $path = mysqli_real_escape_string($connection, $_POST["userpath"]);
+  $status = mysqli_real_escape_string($connection, $_POST["userstatus"]);
 
-if (isset($_POST['register'])) {
-  $fname = mysqli_real_escape_string($conn, $_POST['user_fname']);
-  $lname = mysqli_real_escape_string($conn, $_POST['user_lname']);
-  $usermobile = mysqli_real_escape_string($conn, $_POST['user_mobile']);
-  $usergender = mysqli_real_escape_string($conn, $_POST['user_gender']);
-  $userbday = $_POST['user_dob'];
-  $useraddress = mysqli_real_escape_string($conn, $_POST['u_address']);
-  $useremail = mysqli_real_escape_string($conn, $_POST['user_email']);
-  $userpassword = mysqli_real_escape_string($conn, $_POST['user_pass']);
-  $usertype = mysqli_real_escape_string($conn, $_POST['u_type']);
-
-  if (empty($fname) || empty($lname) || empty($usermobile) || empty($usermobile) || empty($userbday) || empty($useraddress) || empty($fname) || empty($email) || empty($username) || empty($password) || empty($confirm_pass) || empty($usertype)) {
-    $errors['userType'] = "Fill in all the fields";
-  }
-
-  if ($password !== $confirm_pass) {
-    $errors['password_match'] = "Passwords do not match";
-  }
-
-  if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    $errors['email'] = "Invalid email format";
-  }
-
-  if ((count($errors) == 0) && ($isOk)) {
-    $insertQuery = "INSERT INTO users (user_email, user_pass, user_fname, user_lname, user_mobile, user_gender, user_dob, user_address, user_path, user_type, user_role) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+  if (count($errors) == 0) {
+    // Insert user data into the database
+    $insertQuery = "INSERT INTO users (user_email, user_pass, user_fname, user_lname, user_mobile, user_gender, user_dob, user_address, user_type, user_path, user_status) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    $hashedPassword = password_hash($password, PASSWORD_DEFAULT); // Hash the password
     $stmt = $connection->prepare($insertQuery);
-    $stmt->bind_param('ssssss', $email, $username, $hashedPassword);
+    $stmt->bind_param('sssssssssss', $email, $hashedPassword, $fname, $lname, $mobilenumber, $gender, $bday, $address, $type, $path, $status);
+
     if ($stmt->execute()) {
       header('location: login.php');
       exit;
@@ -52,136 +36,117 @@ if (isset($_POST['register'])) {
 }
 ?>
 
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Register as a Vendor</title>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
-  <link rel="stylesheet"
-    href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-  <script
-    src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
-  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-  <script src="https://unpkg.com/@popperjs/core@2"></script>
-
-  <?php include('includes/header2.html'); ?>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+    crossorigin="anonymous"></script>
+    <?php include('includes/header2.html'); ?>
 </head>
 
 <body>
   <div class="container">
-    <div class="d-flex justify-content-center align-items-center my-4 ">
+    <div class="d-flex justify-content-center align-items-center my-4">
       <form id="registration" class="w-25" method="POST">
+      <div class="mb-3">
+      <h3>Registration Form</h3>
+      </div>
         <div class="mb-3">
-          <label for="exampleFormControlTextarea1" class="form-label">First Name</label>
+          <label for="fname" class="form-label">
+            First Name
+          </label>
           <input type="text" name="fname" class="form-control">
         </div>
         <div class="mb-3">
-          <label for="exampleFormControlTextarea1" class="form-label">Last Name</label>
+          <label for="lname" class="form-label">
+            Last Name
+          </label>
           <input type="text" name="lname" class="form-control">
         </div>
         <div class="mb-3">
-          <label for="exampleFormControlTextarea1" class="form-label">Mobile Number</label>
-          <input type="text" name="usermobile" class="form-control">
+          <label for="mobilenumber" class="form-label">
+            Mobile Number
+          </label>
+          <input type="text" name="mobilenumber" class="form-control">
         </div>
         <div class="mb-3">
+          <label for="gender" class="form-check-label">Gender</label>
           <div class="form-check">
-            <input class="form-check-input" type="radio" name="usergender" value="Male" id="flexRadioDefault1">
-            <label class="form-check-label" for="flexRadioDefault1">
+            <input class="form-check-input" type="radio" name="gender" value="Male" id="genderMale">
+            <label class="form-check-label" for="gender">
               Male
             </label>
           </div>
           <div class="form-check">
-            <input class="form-check-input" type="radio" name="usergender" value="Female" id="flexRadioDefault2"
-              checked>
-            <label class="form-check-label" for="flexRadioDefault2">
+            <input class="form-check-input" type="radio" name="gender" value="Female" id="genderFemale">
+            <label class="form-check-label" for="gender">
               Female
             </label>
           </div>
           <div class="form-check">
-            <input class="form-check-input" type="radio" name="usergender" value="Rather not say" id="flexRadioDefault2"
-              checked>
-            <label class="form-check-label" for="flexRadioDefault2">
-              Rather not say
+            <input class="form-check-input" type="radio" name="gender" value="Other" id="genderOther">
+            <label class="form-check-label" for="gender">
+              Other
             </label>
           </div>
         </div>
         <div class="mb-3">
           <label for="datepicker" class="form-label">Date of Birth</label>
-          <input type="date" name="user_dob" class="form-control datepicker" id="datepicker">
+          <input type="date" name="bday" class="form-control datepicker" id="datepicker">
         </div>
         <div class="mb-3">
-          <label for="exampleFormControlTextarea1" class="form-label">Address</label>
-          <input type="text" name="Address" class="form-control">
+          <label for="address" class="form-label">
+            Address
+          </label>
+          <input type="text" name="address" class="form-control">
         </div>
         <div class="mb-3">
           <label for="imageInput" class="form-label">Choose an image:</label>
           <input type="file" class="form-control" id="imageInput" name="image">
         </div>
         <div class="mb-3">
-          <label for="exampleFormControlInput1" class="form-label">Email address</label>
-          <input type="email" name="useremail" class="form-control" id="exampleFormControlInput1"
-            placeholder="name@example.com">
-          <?php if (isset($errors['email'])): ?>
-            <div class="text-danger">
-              <?php echo $errors['email']; ?>
-            </div>
-          <?php endif; ?>
+          <label for="email" class="form-label">
+            Email
+          </label>
+          <input type="email" name="email" class="form-control" placeholder="name@example.com">
         </div>
         <div class="mb-3">
-          <label for="exampleFormControlTextarea1" class="form-label">Username</label>
-          <input type="text" name="username" class="form-control">
+          <label for="password" class="form-label">
+            Password
+          </label>
+          <input type="password" name="password" class="form-control">
         </div>
         <div class="mb-3">
-          <label for="exampleFormControlTextarea1" class="form-label">Password</label>
-          <input type="password" name="userpassword" class="form-control">
+          <label for="confirmpass" class="form-label">
+            Confirm Password
+          </label>
+          <input type="password" name="confirmpassword" class="form-control">
         </div>
         <div class="mb-3">
-          <label for="exampleFormControlTextarea1" class="form-label">Confirm Password</label>
-          <input type="password" name="confirm_pass" class="form-control">
-        </div>
-        <div class="mb-3">
-          <div class="form-check form-check-inline">
-            <input class="form-check-input" type="checkbox" id="inlineCheckbox1" name="usertype" value="Customer">
-            <label class="form-check-label" for="inlineCheckbox1">Customer</label>
+          <label for="usertype" class="form-check-label">Register as:</label>
+          <div class="form-check">
+          <input class="form-check-input" type="radio" name="type" value="Customer" id="typeCustomer">
+            <label class="form-check-label" for="usertype">
+              Customer
+            </label>
           </div>
-          <div class="form-check form-check-inline">
-            <input class="form-check-input" type="checkbox" id="inlineCheckbox2" name="usertype" value="Supplier">
-            <label class="form-check-label" for="inlineCheckbox2">Supplier</label>
+          <div class="form-check">
+          <input class="form-check-input" type="radio" name="type" value="Supplier" id="typeSupplier">
+            <label class="form-check-label" for="usertype">
+              Supplier
+            </label>
           </div>
         </div>
-        <button type="submit" name="register" class="btn btn-primary">Submit</button>
-
-        <?php if (count($errors) > 0): ?>
-          <div>
-            <?php foreach ($errors as $error): ?>
-              <li>
-                <?php echo $error; ?>
-              </li>
-            <?php endforeach; ?>
-          </div>
-        <?php endif; ?>
       </form>
-    </div>
-    <div class="text-center mt-3">
-      <p>Already have an account? <a href="login.php">Log in here</a>.</p>
     </div>
   </div>
 
-  <script>
-    $(document).ready(function () {
-      $('#datepicker').datepicker({
-        format: 'yyyy-mm-dd', // Set date format
-        autoclose: true,      // Close the datepicker when a date is selected
-        todayHighlight: true  // Highlight today's date
-      });
-    });
-  </script>
-  <?php include('includes/footer.html'); ?>
+  <?php include('includes/footer.html') ?>
 </body>
 
 </html>
