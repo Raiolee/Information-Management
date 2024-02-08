12bf03b3b64a925afd68f29969831ae7edf6f3d5
Raiolee <114850923+Raiolee@users.noreply.github.com>
2024-02-08 15:38:23 +0800

    Delete SVMS directory

--- SVMS/config/connection.php
+++
@@ -1,7 +0,0 @@
-<?php 
-require 'constants.php';
-
-$connection = mysqli_connect(HOST_NAME, USER_NAME, PASSWORD, DB_NAME);
-
-    if (!$connection) { die("could not connect" . mysqli_connect_error() );}
-?>
